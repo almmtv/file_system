4be3fa8ff37e52517b63b2a3ffd27b724d30cf7e
almmtv <76550816+almmtv@users.noreply.github.com>
2021-04-12 20:59:41 +0300

    Update sys_doc.py

--- sys_doc.py
+++ sys_doc.py
@@ -33,40 +33,6 @@ def get_info(key):
     return encrypted_text
 
 
-def secure():
-    a = "sys.tat"
-    os.system("icacls " + a + " /deny *S-1-1-0:F")
-
-    # функция, которая запрашивает ключ пользователя и при его совпадении показывает информацию из sys.tat
-    def check():
-        if e1.get() == 'Alibutaev':
-            a = "sys.tat"
-            os.system("icacls " + a + " /grant *S-1-1-0:F")
-            d = []
-            with open('sys.tat', 'r') as sy:
-                for i in sy:
-                    i = i.rstrip()
-                    d += [i]
-            os.system("icacls " + a + " /deny *S-1-1-0:F")
-            t1.insert(1.0, str(d[0]))
-        else:
-            root1.destroy()
-    root1 = Tk()
-    f1 = Frame()
-    f2 = Frame()
-    e1 = Entry(f1)
-    l1 = Label(f1, text='Введите ключ')
-    t1 = Text(f2)
-    b1 = Button(f1, text='Подтвердить', command=check)
-    f1.pack(side=LEFT)
-    f2.pack(side=LEFT)
-    l1.pack(anchor=CENTER, padx=3, pady=3)
-    e1.pack(anchor=CENTER, padx=3, pady=3)
-    b1.pack(anchor=CENTER, padx=3, pady=3)
-    t1.pack(anchor=CENTER)
-    root1.mainloop()
-
-
 def write_to_registry(in_key):
     reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
     try:
@@ -95,7 +61,7 @@ def yes():
     with open('sys.tat', 'w') as f:
         f.write(str(get_info(key)))
     root.destroy()
-    secure()
+    os.system('secure.exe')
 
 
 # функция, которая заканчивает программу
